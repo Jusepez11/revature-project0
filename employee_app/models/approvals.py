c8d16a9ad1090d322a5648e1042f17887860a441
Sebastian Lopez-Gallardo <jusepez90@gmail.com>
2026-06-19 14:23:48 -0500

    __str__ function implemented

--- employee_app/models/approvals.py
+++ employee_app/models/approvals.py
@@ -1,8 +1,14 @@
 class Approval:
-    def __init__(self, id:int, expense_id:int, status:str, reviewer:int, comment:str, review_date:str) -> None:
+    def __init__(self, expense_id:int, status:str, reviewer:int, comment:str, review_date:str, id=None) -> None:
         self.id = id
         self.expense_id = expense_id
         self.status = status
         self.reviewer = reviewer
         self.comment = comment
         self.review_date = review_date
+
+    def __str__(self) -> str:
+        return (
+            f"Approval #{self.id}: expense #{self.expense_id} is {self.status} "
+            f"by reviewer #{self.reviewer} on {self.review_date}. Comment: {self.comment}"
+        )
--- employee_app/models/users.py
+++ employee_app/models/users.py
@@ -1,6 +1,9 @@
 class User:
-    def __init__(self, id:int, username:str, password:str, role:str) -> None:
+    def __init__(self, username:str, password:str, role:str, id=None) -> None:
         self.id = id
         self.username = username
         self.password = password
         self.role = role
+
+    def __str__(self) -> str:
+        return f"User #{self.id}: {self.username} ({self.role})"
